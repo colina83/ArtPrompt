b092504925de2a25655d580f93d27eeecd538858
Francisco.Colina <franco.collina@gmail.com>
2023-07-10 13:06:03 -0500

    Added OpenAI (GPT) response to chatbot

--- ArtLLM/Prompt/views.py
+++ ArtLLM/Prompt/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+import openai
+
+OPENAIKEY ='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'
+openai.api_key = OPENAIKEY
+
+def get_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 150,
+        temperature = 0.9,
+        n=1,
+        stop=None,
+    )
+    #print(response) # Just to check the output of openai
+    anwser = response.choices[0].text.strip()
+    return anwser
 
 # Create your views here.
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'Hi this is me'
+        response = get_openai(message)
         return JsonResponse({'message':message,'response': response})
     return render(request, 'chatbot.html')
